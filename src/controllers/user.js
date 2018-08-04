50286e7101aabe6923d68cb897b50cc4d3b0578a
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2018-08-04 11:29:11 -0300

    Adição de validação do usuário ao autenticar

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -7,7 +7,29 @@ const repository = require('../repositories/user-repository');
 
 exports.authenticate = async (req, res, next) => {
     try {
-        let data = req.body;
+
+        let erros = validator.authenticateValidator(req);
+
+        if (erros) {
+            res.status(400).send({
+                erros: erros
+            });
+            return;
+        }
+
+        let {
+            email,
+            password
+        } = req.body;
+        let data = await repository.getUser(email, password);
+
+        if (!data) {
+            res.status(400).send({
+                erros: message.erros.E0013
+            });
+            return;
+        }
+
         const token = await authService.generateToken({
             email: data.email,
             name: data.name
@@ -16,8 +38,7 @@ exports.authenticate = async (req, res, next) => {
         res.status(200).send({
             token: token,
             data: {
-                email: data.email,
-                password: data.password
+                email: data.email
             }
         });
     } catch (e) {
@@ -44,13 +65,13 @@ exports.create = async (req, res, next) => {
 
     } catch (e) {
         res.status(500).send({
-            message: message.erros.E0007
+            message: message.erros.E0009
         });
     }
 };
 
-exports.update = async(req, res, next) =>{
-    try{
+exports.update = async (req, res, next) => {
+    try {
         let erros = validator.updateValidator(req);
         if (erros) {
             res.status(400).send({
@@ -65,7 +86,7 @@ exports.update = async(req, res, next) =>{
             message: message.messages.M0003
         });
 
-    }catch (e) {
+    } catch (e) {
         res.status(500).send({
             message: message.erros.E0010
         });
--- src/controllers/validator/user-validator.js
+++ src/controllers/validator/user-validator.js
@@ -21,3 +21,12 @@ exports.updateValidator = (req) => {
 
     return req.validationErrors();
 };
+
+exports.authenticateValidator = (req) => {
+    req.assert('email', message.E0001).notEmpty();
+    req.assert('email', message.E0003).isEmail();
+    req.assert('password', message.E0002).notEmpty();
+    req.assert('password', message.E0006).len(5, 100);
+
+    return req.validationErrors();
+};
