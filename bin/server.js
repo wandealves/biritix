6e3fc064fb2091322f431ab0442a95e4e6ab8941
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2018-09-15 16:37:41 -0300

    port

--- bin/server.js
+++ bin/server.js
@@ -4,7 +4,7 @@ const app = require('../src/app');
 const http = require('http');
 const debug = require('debug')('biritix:server');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.set('port', port);
 
 const server = http.createServer(app);
